0c16dc94380eeb4d641e8d0ba6cec20a36bc3ab8
IVAN OLUMOSI <ivanolumosi@gmail.com>
2025-09-25 15:43:17 +0300

    cors setup 1

--- backend/server.ts
+++ backend/server.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 /* ------------------  Middleware ------------------ */
 
 app.use(cors({
-  origin: 'https://aminius-insurance-company.onrender.com',
+  origin: 'https://aminius-app.netlify.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
